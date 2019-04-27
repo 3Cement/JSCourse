522cfeca6c68b942a94d885336f9c0724e9cce47
3Cement <danielmilewski123@gmail.com>
2019-04-27 17:46:28 +0200

    Learning about AJAX and REST-API

--- DOMCart/AJAX/AJAX/app.js
+++ DOMCart/AJAX/AJAX/app.js
@@ -9,14 +9,30 @@ function loadData() {
     xhr.open('GET', 'data.txt', true);
 
     // Execution of the ajax call
-    xhr.onload = function() {
-        // Codes
-        // 200: Correct
-        // 400: Forbidden
-        // 404: Not found
-        console.log(this.status);
-        if(this.status === 200) {
-            document.getElementById('output').innerHTML = `<h1>${this.responseText}`;
+    // xhr.onload = function() {
+    //     // Codes
+    //     // 200: Correct
+    //     // 400: Forbidden
+    //     // 404: Not found
+    //     console.log(this.status);
+    //     if(this.status === 200) {
+    //         document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+    //     }
+    // }
+
+    // Another way to make an AJAX call
+    xhr.onreadystatechange = function() {
+        console.log('Ready State', xhr.readyState);
+
+        // Ready States
+        // 0 - Unsent
+        // 1 - Opened
+        // 2 - Received
+        // 3 - Loading
+        // 4 - Done
+
+        if(this.status === 200 && this.readyState === 4) {
+            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
         }
     }
 
