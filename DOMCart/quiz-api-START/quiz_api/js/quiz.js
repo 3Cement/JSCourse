b9a636249724505a30d32e8df2e72d772ad2cb87
3Cement <danielmilewski123@gmail.com>
2019-10-13 20:30:39 +0200

    selecting Answer option added, shows which answer is clicked

--- DOMCart/quiz-api-START/quiz_api/js/quiz.js
+++ DOMCart/quiz-api-START/quiz_api/js/quiz.js
@@ -22,6 +22,7 @@ displayQuestion = questions => {
     questionHTML.classList.add('col-12');
 
     questions.forEach(question => {
+        console.log(question);
 
         // read the correct answer
         correctAnswer = question.correct_answer;
@@ -30,7 +31,39 @@ displayQuestion = questions => {
         let possibleAnswers = question.incorrect_answers;
         possibleAnswers.splice( Math.floor( Math.random() * 3 ), 0, correctAnswer );
 
-        console.log(possibleAnswers);
-        console.log(question);
+        // add the HTML for the Current Question
+        questionHTML.innerHTML = `
+        <div class="row justify-content-between heading">
+            <p class="category">Category: ${question.category}</p>
+            <h2 class="text-center">${question.question}</h2>
+        `;
+
+        // generate the HTML for possible answers
+        const answerDiv = document.createElement('div');
+        answerDiv.classList.add('questions', 'row', 'justify-content-around', 'mt-4')
+        possibleAnswers.forEach(answer => {
+            const answerHTML = document.createElement('li');
+            answerHTML.classList.add('col-12', 'col-md-5');
+            answerHTML.textContent = answer;
+            // attach an event click the answer is clicked
+            answerHTML.onclick = selectAnswer;
+            answerDiv.appendChild(answerHTML);
+        });
+        questionHTML.appendChild(answerDiv);
+
+        // render in the HTML
+        document.querySelector('#app').appendChild(questionHTML);
     });
+}
+
+// when the answer is selected
+selectAnswer = (e) => {
+
+    // removes the previous active class for the answer
+    if(document.querySelector('.active')){
+        const activeAnswer = document.querySelector('.active');
+        activeAnswer.classList.remove('active');
+    }
+    // adds the current answer
+    e.target.classList.add('active');
 }
