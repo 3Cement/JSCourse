cb2472cd9cc17f49317eeaa44acef1eb5107476f
3Cement <danielmilewski123@gmail.com>
2019-10-13 18:53:29 +0200

    Fetch answers from api and put them to the array with correct answer

--- DOMCart/quiz-api-START/quiz_api/js/quiz.js
+++ DOMCart/quiz-api-START/quiz_api/js/quiz.js
@@ -1,5 +1,36 @@
+let correctAnswer;
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('ready');
+    loadQuestion();
 });
+
+// loads a new question from an API
+loadQuestion = () => {
+    const url = 'https://opentdb.com/api.php?amount=1';
+    fetch(url)
+    .then(data => data.json())
+    .then(result => displayQuestion(result.results));
+}
+
+// displays the question HTML from API
+
+displayQuestion = questions => {
+
+    // create the HTML Question
+    const questionHTML = document.createElement('div');
+    questionHTML.classList.add('col-12');
+
+    questions.forEach(question => {
+
+        // read the correct answer
+        correctAnswer = question.correct_answer;
+
+        // inject the correct answer in the possible answers
+        let possibleAnswers = question.incorrect_answers;
+        possibleAnswers.splice( Math.floor( Math.random() * 3 ), 0, correctAnswer );
+
+        console.log(possibleAnswers);
+        console.log(question);
+    });
+}
