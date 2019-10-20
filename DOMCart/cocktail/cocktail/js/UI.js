2f895b88b86c8df459499e419750e8508681f241
3Cement <danielmilewski123@gmail.com>
2019-10-20 14:07:20 +0200

    Create app.js and UI.js for coctails

---
+++ DOMCart/cocktail/cocktail/js/UI.js
@@ -0,0 +1,26 @@
+class UI {
+
+
+    // Displays a Custom Message
+    printMessage(message, className) {
+        const div = document.createElement('div');
+
+        // Add the HTML
+        div.innerHTML = `
+            <div class="alert alert-dismissible alert-${className}">
+                <button type="button" class="close" data-dismiss="alert">X</button>
+                ${message}
+            </div>
+        `;
+
+        // Insert before
+        const reference = document.querySelector('.jumbotron h1');
+        const parentNode = reference.parentElement;
+        parentNode.insertBefore(div, reference);
+
+        // remove after 3 seconds
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+}
---
+++ DOMCart/cocktail/cocktail/js/app.js
@@ -0,0 +1,31 @@
+// Instanciate the Classes
+const ui = new UI();
+
+
+// Create the Event Listeners
+function eventListeners() {
+
+    // Add event listener when form is submitted
+    const searchForm = document.querySelector('#search-form');
+    if(searchForm) {
+        searchForm.addEventListener('submit', getCocktails);
+    }
+}
+
+eventListeners();
+
+
+// Get cocktails function
+function getCocktails(e) {
+    e.preventDefault();
+
+    const searchTerm = document.querySelector('#search').value;
+
+    // Check something is on the search input
+    if(searchTerm === '') {
+        // Call User Interface print message
+        ui.printMessage('Please add something into the form', 'danger');
+    } else {
+        console.log('Query the REST API');
+    }
+}
