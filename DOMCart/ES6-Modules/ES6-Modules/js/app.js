7150807aab03f91e738c8b8ceab1b90d0d416afd
3Cement <danielmilewski123@gmail.com>
2019-10-30 20:51:01 +0100

    Export functions with two methods, by const or function

--- DOMCart/ES6-Modules/ES6-Modules/js/app.js
+++ DOMCart/ES6-Modules/ES6-Modules/js/app.js
@@ -1,10 +1,14 @@
 // import from client.js
-import {clientName, amount} from './client.js'
+import {clientName, amount, displayClientInfo} from './client.js'
 // import * as clientInfo from './client.js'
 
 // console.log(clientInfo)
 // console.log(clientInfo.clientName);
 // console.log(clientInfo.amount)
 
-console.log(clientName);
-console.log(amount);
+// console.log(clientName);
+// console.log(amount);
+
+// use the function
+let client = displayClientInfo(clientName, amount);
+console.log(client);
--- DOMCart/ES6-Modules/ES6-Modules/js/client.js
+++ DOMCart/ES6-Modules/ES6-Modules/js/client.js
@@ -1,2 +1,10 @@
 export const clientName = 'Juan';
 export let amount = 200;
+
+// export function displayClientInfo(name, amount) {
+//     return `Client: ${name} - Amount: ${amount}`;
+// }
+
+export const displayClientInfo = (name, amount) => {
+    return `Client: ${name} - Amount: ${amount}`;
+}
